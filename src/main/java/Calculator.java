e02345e589c9d4f318ff8a158e1085f9c109d462
flynn <flynn@LaRed>
2018-03-15 11:45:02 +0100

    Prueba1

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,22 @@
+
+public class Calculator {
+
+	private int operand1, operand2;
+	
+	public Calculator() {}
+	
+	public Calculator(int op1, int op2) {
+		this.operand1 = op1;
+		this.operand2 = op2;
+	}
+	
+	public void setOperandsInMemory(int op1, int op2) {
+		this.operand1 = op1;
+		this.operand2 = op2;
+	}
+	
+	public int add() {
+		return this.operand1 + this.operand2;
+	}
+
+}
